ed26bb82fee475456effbf907d04ad93e66a9664
Thibaut <thibautstachnick@gmail.com>
2024-07-13 11:58:42 +0200

    add a new component for squared bento

--- components/BentoSquare.vue
+++ components/BentoSquare.vue
@@ -1,9 +1,26 @@
 <script setup lang="ts">
-
+const props = defineProps({
+  title:{
+    type: String,
+    required:true,
+  },
+  content:{
+    type: String,
+    default:'nothing to show',
+    required: false
+  }
+})
 </script>
 
 <template>
-  <Button label="Check" icon="pi pi-check" />
+  <Card class="w-1/3 h-1/3 m-2.5">
+    <template #title>{{ title }}</template>
+    <template #content>
+      <p class="m-0.5">
+        {{ content }}
+      </p>
+    </template>
+  </Card>
 </template>
 
 <style scoped>
