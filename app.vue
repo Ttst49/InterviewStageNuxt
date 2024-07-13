0d1b8f1ad036819e6203d9edefe140a3e48a52b4
Thibaut <thibautstachnick@gmail.com>
2024-07-14 01:49:00 +0200

    add a way to call pages by nuxt router

--- app.vue
+++ app.vue
@@ -1,50 +1,5 @@
 <template>
-  <div>
-    <div class="w-100 d-flex flex-row">
-      <BentoSquare
-          @open-bento="openBento"
-          class="col-3"
-          title="coucou"
-          size="sm"
-      />
-      <BentoSquare
-          @open-bento="openBento"
-          class="col-3"
-          title="coucou"
-          size="sm"
-      />
-      <BentoSquare
-          @open-bento="openBento"
-          class="col-3"
-          title="coucou"
-          size="sm"
-      />
-    </div>
-    <BentoSquare
-        @open-bento="openBento"
-        title="salut"
-        size="lg"
-    />
-    <BentoSquare
-        @open-bento="openBento"
-        title="hey hay"
-        size="md"
-    />
-  </div>
+  <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
 </template>
-<script setup lang="ts">
-import { defineProps } from 'vue';
-
-const props = defineProps({
-  visible: {
-    default: false,
-    type: Boolean
-  }
-});
-
-const emit = defineEmits(['update:visible']);
-
-
-const openBento = () => {
-};
-</script>
