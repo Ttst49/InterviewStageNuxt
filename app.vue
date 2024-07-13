42994544a9dae8d61a6db7fe0aa407d81c7b0d0b
Thibaut <thibautstachnick@gmail.com>
2024-07-13 13:32:25 +0200

    update the bentoSquare component and add some to app.vue for test purpose

--- app.vue
+++ app.vue
@@ -1,13 +1,50 @@
 <template>
   <div>
+    <div class="w-100 d-flex flex-row">
+      <BentoSquare
+          @open-bento="openBento"
+          class="col-3"
+          title="coucou"
+          size="sm"
+      />
+      <BentoSquare
+          @open-bento="openBento"
+          class="col-3"
+          title="coucou"
+          size="sm"
+      />
+      <BentoSquare
+          @open-bento="openBento"
+          class="col-3"
+          title="coucou"
+          size="sm"
+      />
+    </div>
     <BentoSquare
-        title="coucou"
-    />
-    <BentoSquare
+        @open-bento="openBento"
         title="salut"
+        size="lg"
     />
     <BentoSquare
+        @open-bento="openBento"
         title="hey hay"
+        size="md"
     />
   </div>
 </template>
+<script setup lang="ts">
+import { defineProps } from 'vue';
+
+const props = defineProps({
+  visible: {
+    default: false,
+    type: Boolean
+  }
+});
+
+const emit = defineEmits(['update:visible']);
+
+
+const openBento = () => {
+};
+</script>
--- components/BentoSquare.vue
+++ components/BentoSquare.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+
 const props = defineProps({
   title:{
     type: String,
@@ -8,21 +9,53 @@ const props = defineProps({
     type: String,
     default:'nothing to show',
     required: false
+  },
+  size:{
+    default: "sm",
+    type:String
   }
 })
+
+const getSize = computed(() => {
+  switch (props.size) {
+    case "sm":
+      return "h-48 w-48 bg-sky-500 m-1 bentoSquare";
+    case "md":
+      return "h-64 w-64 bg-sky-700 m-1 bentoSquare";
+    case "lg":
+      return "h-80 w-80 bg-sky-900 m-1 bentoSquare";
+    default:
+      return "";
+  }
+});
+
+
+defineEmits({
+  "open-bento": value => value
+})
+
 </script>
 
 <template>
-  <Card class="w-1/3 h-1/3 m-2.5">
+  <Card
+      @click="$emit('open-bento',content)"
+      :class="getSize"
+  >
     <template #title>{{ title }}</template>
     <template #content>
-      <p class="m-0.5">
+      <p
+          class="m-0.5">
         {{ content }}
       </p>
+      {{size}}
     </template>
   </Card>
 </template>
 
 <style scoped>
-
+.bentoSquare:hover{
+  translate: -5px -5px 0;
+  border: whitesmoke 1px groove;
+  transition: .5s ease-out translate;
+}
 </style>
