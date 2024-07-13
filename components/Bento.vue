cff68bbea466a2ec97238b602b4be7a143e9f6c0
Thibaut <thibautstachnick@gmail.com>
2024-07-14 01:50:18 +0200

    update component Bento and add more options, add a PresentationText component that define a new way to display text into the app

--- components/Bento.vue
+++ components/Bento.vue
@@ -1,61 +1,93 @@
-<script setup lang="ts">
+<template>
+  <Card
+      @click="$emit('open-bento', miniContent)"
+      :class="[
+          getSize,
+      'm-3 bento cursor-pointer',
+      { 'focused': isFocus }
+      ]"
+      @focus="isFocus = true"
+      @blur="isFocus = false"
+      tabindex="0"
+  >
+    <template #title>{{ title }}</template>
+    <template #content>
+      <transition name="fade">
+      <p class="m-0.5">{{ isFocus ? fullContent : miniContent }} </p>
+      </transition>
+    </template>
+  </Card>
+</template>
 
+<script setup lang="ts">
 const props = defineProps({
-  title:{
+  title: {
     type: String,
-    required:true,
+    required: true,
   },
-  content:{
+  miniContent: {
     type: String,
-    default:'nothing to show',
-    required: false
+    default: "Rien par ici",
+    required: false,
   },
-  size:{
+  fullContent:{
+    type:String,
+    default:"Rien par ici"
+  },
+  size: {
     default: "sm",
-    type:String
-  }
-})
+    type: String,
+  },
+  isRectangle: {
+    default: false,
+    type: Boolean,
+  },
+});
 
 const getSize = computed(() => {
+  if (props.isRectangle) {
+    switch (props.size) {
+      case "sm":
+        return "h-40 w-64";
+      case "md":
+        return "h-48 w-80";
+      case "lg":
+        return "h-64 w-96";
+      default:
+        return "";
+    }
+  }
   switch (props.size) {
     case "sm":
-      return "h-48 w-48 bg-sky-500 m-1 bentoSquare";
+      return "h-48 w-48";
     case "md":
-      return "h-64 w-64 bg-sky-700 m-1 bentoSquare";
+      return "h-64 w-64";
     case "lg":
-      return "h-80 w-80 bg-sky-900 m-1 bentoSquare";
+      return "h-80 w-80";
     default:
       return "";
   }
 });
 
-
 defineEmits({
-  "open-bento": value => value
-})
+  "open-bento": (value) => value,
+});
 
+const isFocus = ref(false);
 </script>
 
-<template>
-  <Card
-      @click="$emit('open-bento',content)"
-      :class="getSize"
-  >
-    <template #title>{{ title }}</template>
-    <template #content>
-      <p
-          class="m-0.5">
-        {{ content }}
-      </p>
-      {{size}}
-    </template>
-  </Card>
-</template>
-
 <style scoped>
-.bentoSquare:hover{
-  translate: -5px -5px 0;
-  border: whitesmoke 1px groove;
-  transition: .5s ease-out translate;
+.bento {
+  transition: 0.5s ease-out transform, 0.5s ease-out width;
+}
+.bento:focus{
+  transform: translate(-5px, -5px);
+  border: 1px groove whitesmoke;
+  height: 100%;
+  width: 98%;
+}
+
+.fade-enter-active, .fade-leave-active {
+  transition: opacity 0.5s;
 }
 </style>
--- components/PresentationText.vue
+++ components/PresentationText.vue
@@ -1,11 +1,32 @@
 <script setup lang="ts">
-
+defineProps({
+  content:{
+    required: true
+  }
+})
 </script>
 
 <template>
-  $END$
+  <div class="m-3.5 w-100 flex justify-center items-center typewriter">
+    <h1
+        class="h-full text-5xl"
+    >
+    {{content}}
+  </h1>
+  </div>
 </template>
 
 <style scoped>
+.typewriter h1{
+  overflow: hidden;
+  white-space: nowrap;
+  letter-spacing: .15em;
+  animation:
+      typing 2.5s steps(40, end) forwards;
+}
 
+@keyframes typing {
+  from { width: 0 }
+  to { width: 100% }
+}
 </style>
