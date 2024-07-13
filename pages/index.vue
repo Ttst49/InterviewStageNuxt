5aa1adeb7ee21d9e17d20ace59f3b610040692a5
Thibaut <thibautstachnick@gmail.com>
2024-07-14 01:48:29 +0200

    update pages and their locations

---
+++ pages/index.vue
@@ -0,0 +1,63 @@
+<template>
+  <div>
+    <Bento
+        @open-bento="openBento"
+        class="col-3"
+        title="coucou"
+        size="sm"
+        :is-rectangle="false"
+    />
+    <Bento
+        @open-bento="openBento"
+        class="col-3"
+        title="coucou"
+        size="sm"
+        :is-rectangle="true"
+    />
+    <Bento
+        @open-bento="openBento"
+        class="col-3"
+        title="coucou"
+        size="md"
+        :is-rectangle="false"
+    />
+    <Bento
+        @open-bento="openBento"
+        class="col-3"
+        title="coucou"
+        size="md"
+        :is-rectangle="true"
+    />
+    <Bento
+        @open-bento="openBento"
+        class="col-3"
+        title="coucou"
+        size="lg"
+        :is-rectangle="false"
+    />
+    <Bento
+        @open-bento="openBento"
+        class="col-3"
+        title="coucou"
+        size="lg"
+        :is-rectangle="true"
+    />
+  </div>
+</template>
+<script setup lang="ts">
+import { defineProps } from 'vue';
+import Bento from "~/components/Bento.vue";
+
+const props = defineProps({
+  visible: {
+    default: false,
+    type: Boolean
+  }
+});
+
+defineEmits(['update:visible']);
+
+
+const openBento = () => {
+};
+</script>
